0e88a1e528d31f4d6a827557f020e94a08769654
les7171 <62687909+les7171@users.noreply.github.com>
2021-07-01 10:54:07 +0300

    start

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('display_errors', "1");
+ini_set("error_log", __DIR__ . "/error.log");
+error_reporting(E_ALL);
+define('BASE_PATH', __DIR__);
+require_once __DIR__ . '/application/bootstrap.php';
